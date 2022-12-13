18c5378712df3ddbda563cc8f0607bee0bbf8ca0
Tim Fischbach <tfischbach@codevise.de>
2022-12-13 22:36:59 +0100

    Do not ignore personal/custom.el

---
+++ personal/custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ido-use-filename-at-point nil)
+ '(js-indent-level 2)
+ '(nxml-attribute-indent 2)
+ '(nxml-child-indent 2)
+ '(package-selected-packages
+   (quote
+    (typescript-mode prettier-js php-mode multi-term multi-vterm elixir-mode rspec-mode projectile-rails counsel swiper ivy yaml-mode web-mode tide scss-mode yari inf-ruby lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company helm-projectile exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diff-hl discover-my-major crux anzu ag ace-window tabbar session pod-mode muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode folding eproject diminish csv-mode browse-kill-ring boxquote bm bar-cursor apache-mode)))
+ '(rspec-spec-command "bin/rspec")
+ '(rspec-use-bundler-when-possible nil)
+ '(rspec-use-spring-when-possible nil)
+ '(sp-override-key-bindings (quote (("C-<left>") ("C-<right>"))))
+ '(typescript-indent-level 2)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-markup-indent-offset 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t nil))))
